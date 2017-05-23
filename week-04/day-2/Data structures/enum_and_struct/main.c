aa6826707c9aacee86d2aec14c136492a70e357e
Mate Lengyel <lengyel.mate@gmail.com>
2017-05-23 18:01:46 +0200

    data structures almost done

--- week-04/day-2/Data structures/enum_and_struct/main.c
+++ week-04/day-2/Data structures/enum_and_struct/main.c
@@ -18,11 +18,24 @@ typedef struct {
 // The market price of the houses is 400 EUR / square meters
 // Create a function that takes a pointer to a house and decides if it's worth to buy
 // (if the price is lower than the calculated price from it's area)
-
+bool worth_to_buy(house *h)
+{
+    if (h->price < MARKET_PRICE)
+        return true;
+    return false;
+}
 // TODO:
 // Create a function that takes an array of houses (and it's length), and counts the
 // houses that are worth to buy.
+int good_houses(house *array, int length)
+{
+    int cntr;
 
+    for (int i = 0; i < length; i++) {
+        if(worth_to_buy(*array[i]))
+            cntr++;
+    }
+}
 
 
 
